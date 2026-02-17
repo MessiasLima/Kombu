46978d0f8146410ce1547c07184f0b179b47fb32
Messias Junior <messiaslima.03@gmail.com>
2026-02-17 13:13:51 +0000

    feat: enhance MainScreen with adaptive navigation and improved padding logic

--- feature/main/impl/src/commonMain/kotlin/dev/appoutlet/kombu/feature/main/impl/MainScreen.kt
+++ feature/main/impl/src/commonMain/kotlin/dev/appoutlet/kombu/feature/main/impl/MainScreen.kt
@@ -1,15 +1,23 @@
 package dev.appoutlet.kombu.feature.main.impl
 
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation3.runtime.NavKey
 import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.runtime.rememberNavBackStack
 import androidx.navigation3.ui.NavDisplay
 import androidx.savedstate.serialization.SavedStateConfiguration
+import androidx.window.core.layout.WindowSizeClass.Companion.WIDTH_DP_MEDIUM_LOWER_BOUND
 import dev.appoutlet.kombu.core.navigation.getSavedStateConfiguration
 import dev.appoutlet.kombu.feature.websites.WebsitesDestination
 import kotlinx.serialization.modules.SerializersModule
@@ -22,11 +30,12 @@ fun MainScreen() {
     val navigationAggregator = koinInject<MainTabNavigationAggregator>()
     val config = remember(navigationAggregator) { getSavedStateConfiguration(navigationAggregator.navigation) }
     val backStack = rememberNavBackStack(configuration = config, WebsitesDestination)
-
+    val itemPaddingBottom by animateDpAsState(targetValue = if (isNarrowScreen()) 0.dp else 12.dp)
     NavigationSuiteScaffold(
         navigationSuiteItems = {
             navigationAggregator.navigation.forEach { item ->
                 item(
+                    modifier = Modifier.padding(bottom = itemPaddingBottom),
                     selected = backStack.last() == item.key,
                     onClick = {
                         if (item.key == WebsitesDestination) {
@@ -54,4 +63,13 @@ fun MainScreen() {
             )
         }
     )
+}
+
+
+@Composable
+private fun isNarrowScreen(): Boolean {
+    val windowSizeClass = currentWindowAdaptiveInfo().windowSizeClass
+    return remember(windowSizeClass) {
+        windowSizeClass.isWidthAtLeastBreakpoint(WIDTH_DP_MEDIUM_LOWER_BOUND).not()
+    }
 }
