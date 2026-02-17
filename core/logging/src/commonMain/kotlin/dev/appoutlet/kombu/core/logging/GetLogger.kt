3d81fe2cf39795e32135c5bd190f1b958b98cbc2
Messias Junior <messiaslima.03@gmail.com>
2026-02-17 12:26:44 +0000

    feat: refactor logger initialization for improved readability

--- core/logging/src/commonMain/kotlin/dev/appoutlet/kombu/core/logging/GetLogger.kt
+++ core/logging/src/commonMain/kotlin/dev/appoutlet/kombu/core/logging/GetLogger.kt
@@ -4,12 +4,12 @@ import co.touchlab.kermit.Logger
 import co.touchlab.kermit.mutableLoggerConfigInit
 import co.touchlab.kermit.platformLogWriter
 
-fun getLogger(tag: String): Logger {
-    val loggerConfig = mutableLoggerConfigInit(
-        logWriters = arrayOf(
-            platformLogWriter(),
-        ),
-    )
+private val loggerConfig = mutableLoggerConfigInit(
+    logWriters = arrayOf(
+        platformLogWriter(),
+    ),
+)
 
+fun getLogger(tag: String): Logger {
     return Logger(loggerConfig).withTag(tag)
 }
